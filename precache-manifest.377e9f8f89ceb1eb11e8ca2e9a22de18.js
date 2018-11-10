88dc1181f6008afc9c7339889c1d85aea1d7a5c9
Jianyuan Chen <jianyuan.chen12@gmail.com>
2018-11-10 14:52:19 -0800

    Updates

---
+++ precache-manifest.377e9f8f89ceb1eb11e8ca2e9a22de18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "903b105faf55c6f1c4b4",
+    "url": "/static/js/runtime~main.903b105f.js"
+  },
+  {
+    "revision": "9614d1e21b583d239cf2",
+    "url": "/static/js/main.9614d1e2.chunk.js"
+  },
+  {
+    "revision": "3e917add6e070e7af8a4",
+    "url": "/static/js/2.3e917add.chunk.js"
+  },
+  {
+    "revision": "68891ec8beafbd063a2e",
+    "url": "/static/js/1.68891ec8.chunk.js"
+  },
+  {
+    "revision": "9614d1e21b583d239cf2",
+    "url": "/static/css/main.be4fb451.chunk.css"
+  },
+  {
+    "revision": "68891ec8beafbd063a2e",
+    "url": "/static/css/1.101a1746.chunk.css"
+  },
+  {
+    "revision": "b9374f20f716c590db050b2d45708408",
+    "url": "/index.html"
+  }
+];
